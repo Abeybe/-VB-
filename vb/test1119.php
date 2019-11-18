05db6dc51ad518308a20b567e8727110864a0e77
Y Abe <57864880+Abeybe@users.noreply.github.com>
2019-11-18 21:02:55 +0900

    Add files via upload

---
+++ vb/test1119.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<head>
+    <title>jsとphp</title>
+</head>
+<body>
+    <script>
+        //ここがjavascript
+        document.write("hello javascript<br>");
+    </script>
+    
+    <?php 
+        //ここがphp
+        echo "hello php<br>";
+    ?>
+
+    <table border="1">
+        <tr>
+            <th>A</th>
+            <th>B</th>
+            <th>C</th>
+        </tr>
+        <tr>
+            <td>A1</td>
+            <td>B1</td>
+            <td>C1</td>
+        </tr>
+        <tr>
+            <td>A2</td>
+            <td>B2</td>
+            <td>C2</td>
+        </tr>
+        <tr>
+            <td>A3</td>
+            <td>B3</td>
+            <td>C3</td>
+        </tr>
+    </table>
+</body>
+</html>
