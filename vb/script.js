99300b8e6330c2142b783f65d44b01b199a38387
Y Abe <57864880+Abeybe@users.noreply.github.com>
2019-11-19 12:07:13 +0900

    Update script.js

--- vb/script.js
+++ vb/script.js
@@ -1,5 +1,6 @@
-
+//関数(ここでは動作のこと)を定義
 function onclickSayHello(){
+    //htmlからid"sayhello"を取得、クリックされたらalert関数を呼び出す。
     document.getElementById("sayhello").onclick=function(){
         alert("Hello");
     }
